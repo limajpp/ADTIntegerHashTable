2c4a6ed35ec5fee252e02c467540c152ad14f172
João Pedro Lima <joaopedroca90@gmail.com>
2024-11-09 17:52:11 -0300

    feat: "ADTIntegerHashTable implementation with a debugging class"

--- src/debugging/Main.java
+++ src/debugging/Main.java
@@ -1,7 +1,31 @@
-package entities;
+package debugging;
+
+import entities.ADTIntegerHashTable;
 
 public class Main {
     public static void main(String[] args) {
-        
+        ADTIntegerHashTable hashTable = new ADTIntegerHashTable(10);
+
+        System.out.println(hashTable);
+
+        hashTable.insert(35);
+        hashTable.insert(42);
+        hashTable.insert(99);
+        hashTable.insert(87);
+        hashTable.insert(14);
+        hashTable.insert(1);
+
+        System.out.println(hashTable);
+
+        System.out.println(hashTable.contains(5));
+        System.out.println(hashTable.contains(35) + "\n");
+
+        hashTable.remove(99);
+        hashTable.remove(42);
+
+        System.out.println(hashTable);
+
+        System.out.println(hashTable.search(3));
+        System.out.println(hashTable.search(4));
     }
 }
